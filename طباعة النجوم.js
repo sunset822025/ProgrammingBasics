2ca872afc8ad80739e4617de38d46ad71d11040c
sunset822025 <suha47230@gmail.com>
2025-04-27 22:03:59 +0300

    Add files via upload

---
+++ طباعة النجوم.js
@@ -0,0 +1,8 @@
+let rows = prompt('Pleas Enter rows count');
+for(let row =1;row <=rows ; row++){
+    let stars ='';
+    for(let i =0;i<row;i++){
+        stars +='*';
+    }
+    console.log(stars);
+}
