9955d3a6ede5cd01fa32e0d991c4647d0f853bc8
sunset822025 <suha47230@gmail.com>
2025-04-27 14:43:39 +0300

    Add files via upload

---
+++ جمع عناصر المصفوفه.js
@@ -0,0 +1,20 @@
+let cart = [10,25,15,20];
+let total = 0;
+for(let i = 0 ; i<cart.length;i++){
+    
+    total=total+cart[i];
+
+}
+console.log(total);
+
+let c=[10,25,15,20];
+let s=0;
+let x =0;
+while( x <cart.length){
+  
+    s=s+c[x];
+    x++;
+  
+}
+
+console.log(total);
